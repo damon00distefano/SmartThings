1ad8ab08516401d0b150445e141e334b5051ee0f
damon00distefano <damon00distefano@gmail.com>
2020-04-07 17:46:50 -0500

    Update dome-siren.groovy

--- devicetypes/krlaframboise/dome-siren.src/dome-siren.groovy
+++ devicetypes/krlaframboise/dome-siren.src/dome-siren.groovy
@@ -219,45 +219,47 @@ metadata {
 				backgroundColor: "#ffffff"			
 		}
 				
-		standardTile("playBell1", "device.status", width: 2, height: 2) {
-			state "default", label:'Bell 1', action:"bell1", icon:"st.Seasonal Winter.seasonal-winter-002",backgroundColor: "#99FF99"
-		}
-		
-		standardTile("playBell2", "device.status", width: 2, height: 2) {
-			state "default", label:'Bell 2', action:"bell2", icon:"st.Seasonal Winter.seasonal-winter-002",backgroundColor: "#99FF99"
-		}
-		
-		standardTile("playBell3", "device.status", width: 2, height: 2) {
-			state "default", label:'Bell 3', action:"bell3", icon:"st.Seasonal Winter.seasonal-winter-002",backgroundColor: "#99FF99"
-		}
-		
-		standardTile("playBell4", "device.status", width: 2, height: 2) {
-			state "default", label:'Bell 4', action:"bell4", icon:"st.Seasonal Winter.seasonal-winter-002",backgroundColor: "#99FF99"
-		}
-		
-		standardTile("playBell5", "device.status", width: 2, height: 2) {
-			state "default", label:'Bell 5', action:"bell5", icon:"st.Seasonal Winter.seasonal-winter-002",backgroundColor: "#99FF99"
-		}
-		
-		standardTile("playChime1", "device.status", width: 2, height: 2) {
-			state "default", label:'Chime 1', action:"chime1", icon:"st.Entertainment.entertainment2",backgroundColor: "#CC99CC"
-		}
-		
-		standardTile("playChime2", "device.status", width: 2, height: 2) {
-			state "default", label:'Chime 2', action:"chime2", icon:"st.Entertainment.entertainment2",backgroundColor: "#CC99CC"
-		}
-		
-		standardTile("playChime3", "device.status", width: 2, height: 2) {
-			state "default", label:'Chime 3', action:"chime3", icon:"st.Entertainment.entertainment2",backgroundColor: "#CC99CC"
-		}
-		
-		standardTile("playSiren1", "device.status", width: 2, height: 2) {
-			state "default", label:'Siren 1', action:"siren1", icon:"st.security.alarm.clear",backgroundColor: "#ff9999"
-		}
-		
-		standardTile("playSiren2", "device.status", width: 2, height: 2) {
-			state "default", label:'Siren 2', action:"siren2", icon:"st.security.alarm.clear",backgroundColor: "#ff9999"
-		}
+		standardTile("playCarAlarm", "device.status", width: 2, height: 2) {
+state "default", label:'Car Alarm', action:"siren1", icon:"st.security.alarm.clear",backgroundColor: "#ff3333"
+}
+
+standardTile("playAlarmLow", "device.status", width: 2, height: 2) {
+state "default", label:'Alarm', action:"bell1", icon:"st.security.alarm.clear",backgroundColor: "#ff3333"
+}
+
+standardTile("playAlarmHigh", "device.status", width: 2, height: 2) {
+state "default", label:'Alarm High', action:"chime3", icon:"st.security.alarm.clear",backgroundColor: "#ff3333"
+}
+
+standardTile("playRedAlert", "device.status", width: 2, height: 2) {
+state "default", label:'Red Alert', action:"bell2", icon:"st.security.alarm.clear",backgroundColor: "#ff6666"
+}
+
+standardTile("playPolice", "device.status", width: 2, height: 2) {
+state "default", label:'Police', action:"bell3", icon:"st.security.alarm.clear",backgroundColor: "#ff6666"
+}
+
+standardTile("playPoliceHigh", "device.status", width: 2, height: 2) {
+state "default", label:'Police High', action:"bell4", icon:"st.security.alarm.clear",backgroundColor: "#ff6666"
+}
+
+standardTile("playBellChime", "device.status", width: 2, height: 2) {
+state "default", label:'Bells', action:"bell5", icon:"st.Seasonal Winter.seasonal-winter-002",backgroundColor: "#99FF99"
+}
+
+standardTile("playWind", "device.status", width: 2, height: 2) {
+state "default", label:'Wind', action:"siren2", icon:"st.Seasonal Winter.seasonal-winter-002",backgroundColor: "#99FF99"
+}
+
+standardTile("playDoorbell", "device.status", width: 2, height: 2) {
+state "default", label:'Doorbell', action:"chime1", icon:"st.Seasonal Winter.seasonal-winter-002",backgroundColor: "#99FF99"
+}
+
+standardTile("playRobobuzz", "device.status", width: 2, height: 2) {
+state "default", label:'Robobuzz', action:"chime2", icon:"st.Entertainment.entertainment2",backgroundColor: "#CC99CC"
+}
+
+
 		
 		standardTile("refresh", "device.refresh", width: 2, height: 2) {
 			state "refresh", label:'Refresh', action: "refresh", icon:"st.secondary.refresh-icon"
@@ -268,9 +270,11 @@ metadata {
 		}		
 				
 		main "status"
-		details(["status", "playAlarm", "turnOff", "refresh", "playSiren1", "playSiren2", "playChime1", "playBell1", "playBell2", "playChime2", "playBell3", "playBell4", "playChime3", "playBell5", "battery"])
-	}
-}
+		details("status", "playAlarm", "turnOff", "refresh",
+"playCarAlarm", "playAlarmLow", "playAlarmHigh",
+"playRedAlert", "playPolice", "playPoliceHigh",
+"playBellChime", "playWind", "playDoorbell",
+"playRobobuzz", "battery") }
 
 // Initializes the health check interval and sends configuration to device the first time it runs.
 def updated() {	
